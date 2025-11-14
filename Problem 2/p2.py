ecc5a1dd79284d0deaeaa4492e247ce888162d9c
John Whalen <jwhalen0130@floridapoly.edu>
2025-11-14 17:36:32 +0000

    Completed Problem 2

---
+++ Problem 2/p2.py
@@ -0,0 +1,37 @@
+from nicegui import ui
+from random import shuffle
+
+EMOJIS = ['🍟', '🍉', '🥯', '😵', '😵‍💫', '😂', '👋', '💸'] * 2
+shuffle(EMOJIS)
+
+buttons = []
+opened = []
+matched = []
+
+def reset_pair(i, j):
+    buttons[i].set_text("❓")
+    buttons[j].set_text("❓")
+    opened.clear()
+
+def handle_click(idx):
+    if idx in matched or idx in opened:
+        return
+    
+    opened.append(idx)
+    buttons[idx].set_text(EMOJIS[idx])   
+    if len(opened) == 2:
+        if EMOJIS[opened[0]] == EMOJIS[opened[1]]:
+            matched.append(opened[0])
+            matched.append(opened[1])
+            opened.clear()
+        else:
+            ui.timer(0.5, lambda: reset_pair(opened[0], opened[1]), once=True)
+    if len(matched) == 16:
+        ui.notify("You win!")
+
+with ui.grid(columns = 4):
+    for i in range(16):
+        button = ui.button("❓", on_click=lambda _, idx = i: handle_click(idx)).style("font-size: 30px").classes("h-30 w-30") 
+        buttons.append(button)
+
+ui.run(title='Memory Game')

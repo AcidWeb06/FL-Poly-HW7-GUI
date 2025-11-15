00afd1792597882c5e2e4f2853048a2586b342e5
John Whalen <jwhalen0130@floridapoly.edu>
2025-11-15 22:35:30 +0000

    Highlighted hash value

--- Problem 1/p1.py
+++ Problem 1/p1.py
@@ -9,13 +9,17 @@ def twistHash():
         h = h * 0x517CC1C7
         h = h & 0xFFFFFFFF
     h = h ^ len(textString)
-    hashValue.text = f"Hash Value: {h}"
+    hashLabel.text = "Hash value:"
+    hashValue.text = f"{h}"
+    hashValue.style("color: black; background-color: yellow")
 
 with ui.column().classes("w-full"):   
     with ui.card().classes("h-65 w-200 self-auto"):
         ui.label("Hashing").style("color: green; font-size: 40px")
         inputBox = ui.input()
-        hashValue = ui.label("Hash value: ").classes("font-mono text-lg")
+        with ui.row():
+            hashLabel = ui.label("Hash value: ").classes("font-mono text-lg")
+            hashValue = ui.label("").classes("font-mono text-lg")
         ui.button("Get Hash", on_click=twistHash, color="blue")
 
 ui.run()
